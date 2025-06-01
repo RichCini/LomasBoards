af030553d8f435544d2e708279807a58a0d4d2a6
RichCini <rich.cini@gmail.com>
2025-05-31 21:41:58 -0400

    Add files via upload

--- Thunder 186/BIOS Extension/optrom.asm
+++ Thunder 186/BIOS Extension/optrom.asm
@@ -1,5 +1,21 @@
+; This is based on the option ROM loader code from the XTRamTest project by
+; David Giller & Adrian Black (https://github.com/ki3v/xtramtest)
+; (file: \inc\option_rom.asm)
+;
 ; code: language=nasm tabSize=8
-; MkExpROM <myrom.bin >myrom.rom
+;
+; ASSEMBLE: nasm optrom.asm -o optrom.bin -l optrom.lst
+;
+; In order to make the option ROM pass muster with the BIOS loader, it
+; requires a valid checksum byte in the last byte of the ROM. I used
+; MkExpRom (https://github.com/Raffzahn/MakeExpROM) to do this. The output
+; file is a valid ROM file. MakeExpRom needs to be compiled with MASM.
+;
+; USAGE: MkExpROM <optrom.bin >optrom.rom
+;
+; TESTING: this ROM code has been tested in the MartyPC emulator
+; (https://github.com/dbalsom/martypc) which gives great flexibility in which
+; ROM sets are loaded into the emulation.
 ; 
 %include "defines.inc"
 
@@ -9,93 +25,100 @@ int19_save 	equ (0x37*4)		; DCh INT37h - FP emulation
 EPROM		equ 8192		; 8K EPROM 
 
 ; ---------------------------------------------------------------------------
-section_save
+; Header: This is the expansion ROM header.
 ; ---------------------------------------------------------------------------
-section .romdata ; MARK: __ .rwdata __
-; ---------------------------------------------------------------------------
-
-option_prompt: 	db 13, 10, "BIOS Extension Test",13, 10
-		db "Press S to skip...",0
-option_skip: 	db "(skipping)", 13, 10, 0
-option_loaded:	db "Extension loaded", 13, 10, 0
-
-; ---------------------------------------------------------------------------
-section_restore ; MARK: __ restore __
-; ---------------------------------------------------------------------------
-
-optrom_sig:	dw	0xAA55
-optrom_size:	db	EPROM/512
-
+optrom_sig:	dw	0xAA55			; magic number
+optrom_size:	db	EPROM/512		; ROM size in 512-byte chunks
 optrom_start:	jmp	install_int19		; entry point for option ROM
-dos_start:	jmp	optionrom		; entry point for loading from DOS
+		db	"EXPANSION ROM TEST FOR LOMAS T186 SYSTEM "
 
+; ---------------------------------------------------------------------------
+; Initialization: This code installs the INT19h handler. It checks for a prior
+;   installation of our code from a cold boot so that if a warm boot occurs
+;   it won't install another handler. Although the INT19h vector is 
+;   automatically trapped at IPL, the user is given the option to skip the
+;   installation of any other parts of the code. Not sure if this is the best
+;   approach at this point, but will leave it for now.
 ;
 install_int19:
 		pushf
 		push	bx
 		push	si
-		push	ds
+		push	ds			; save key entry regs
 
 		xor	bx,bx
 		mov	ds,bx			; set DS to the interrupt table
 ; install check
-		mov	bx, (int19_save*4)
-		mov	si, [int19_vec]
-		cmp	si, int_19_handler	; if the interrupt vector is already set to ours, skip
+		mov	bx,(int19_save*4)	; get old vector address offset for below
+		mov	si,[int19_vec]		; get offset of installed vector
+		cmp	si,int_19_handler	; is it ours? If so, skip installation
 		je	.done
 
-; not installed, so trap INT19h
-		cli				; no interrupts when trapping
-		mov	word [bx], si		; save the original interrupt vector offset
+; INT19h does not point to our code, so we haven't loaded yet.
+		cli				; no interrupts when changing vectors
+		mov	word [bx],si		; save the original interrupt vector offset
 		mov	si, [int19_vec+2]
-		mov	word [bx+2], si		; save the original interrupt vector segment
-		mov	word [int19_vec], int_19_handler	; set the new interrupt vector offset
-		mov	word [int19_vec+2], cs			; set the new interrupt vector segment
+		mov	word [bx+2],si		; save the original interrupt vector segment
+		mov	word [int19_vec],int_19_handler		; set the new interrupt vector offset
+		mov	word [int19_vec+2],cs			; set the new interrupt vector segment
 
 	.done:	
-		pop	ds
+		pop	ds			; clean up stack and exit
 		pop 	si
 		pop	bx
 		popf				; restores interrupt state too
 		retf
 
-bios_puts:
-		mov	ah, 0x0E
+
+; ---------------------------------------------------------------------------
+; BIOS_puts:	simple string print using INT10h
+; ---------------------------------------------------------------------------
+bios_puts:	
+		mov	ah, 0x0E		; TTY out
 	.loop:
-		mov	al, [cs:si]
+		mov	al,[cs:si]
 		inc	si
-		or	al, al
+		or	al,al
 		jz	.done
-		int	0x10
+		int	10h
 		jmp	.loop
 	.done:
 		ret
 
 
+; ---------------------------------------------------------------------------
+; BIOS_read_input:	simple keyboard input using INT16h
+;   46c is ticks_today_lo
+;   maybe need to add STC to allow "no press" testing.
+; ---------------------------------------------------------------------------
 bios_read_input:
-		mov	bx, 3 * 18 			; 3 seconds timeout
+		mov	bx, 3 * 18 		; 3 seconds timeout
 	.delay_keypress:
-		sti					; Enable interrupts so timer can run
-		add	bx, [0x46C]			; Add pause ticks to current timer ticks
+		sti				; Enable interrupts so timer can run
+		add	bx,[0x46C]		; Add pause ticks to current timer ticks
 	.delay:
-		mov	ah, 01h
-		int	16h				; Check for keypress
-		jnz	.keypress			; End pause if key pressed
+		mov	ah,01h
+		int	16h			; Check for keypress
+		jnz	.keypress		; End pause if key pressed
 
-		mov	cx, [0x46C]			; Get current ticks
-		sub	cx, bx				; See if pause is up yet
-		jc	.delay				; Nope
+		mov	cx,[0x46C]		; Get current ticks
+		sub	cx,bx			; See if pause is up yet
+		jc	.delay			; Nope
 
 	.done:
-		cli					; Disable interrupts
+		cli				; Disable interrupts
 		ret
 
 	.keypress:
-		xor	ah, ah
-		int	16h				; Flush keystroke from buffer
+		xor	ah,ah
+		int	16h			; Flush keystroke from buffer
 		jmp	short .done
 
 
+; ---------------------------------------------------------------------------
+; INT19H Handler:	This is the handler registered by the initialization
+;   routine above.
+; ---------------------------------------------------------------------------
 int_19_handler:
 		; pushf
 		; push	ax			; save registers
@@ -106,22 +129,23 @@ int_19_handler:
 		; push	ds
 		; push	es
 
-		xor	ax,ax
-		mov	ds,ax			; set DS to the interrupt table
+		xor	ax,ax			; set DS to the interrupt table
+		mov	ds,ax			; used only if trapping other vectors
 
-; handler installs automatically, but you have the option to not run the extension code
-; here. wonder if it's better to do a trap skip in the ROM initialization rather than here?
-		mov	si, option_prompt
+; Handler installs automatically, but you have the option to not run the extension code
+; here. Wonder if it's better to remove the skip option or follow this approach?
+; There's a 3-second timeout for hitting a key.
+		mov	si,option_prompt	; let people know we're alive
 		call	bios_puts
-		call	bios_read_input
-		cmp	al, 'S'
+		call	bios_read_input		; get a key from the keyboard
+		cmp	al,'S'			; s=skip, any other key loads
 		je	skipload
-		cmp	al, 's'
+		cmp	al,'s'
 		je	skipload
-		jmp	optionrom
+		jmp	loadcode		; OK to load/run the other code
 
 skipload:
-		mov	si, option_skip
+		mov	si,option_skip
 		call	bios_puts
 		; pop 	es
 		; pop	ds
@@ -134,13 +158,46 @@ skipload:
 		int	int19_save		; chain to old INT19h
 		iret
 
-optionrom:
-		mov	si, option_loaded
+
+; ---------------------------------------------------------------------------
+; loadcode:	This is the core of the code. The INT19h reboot trap is
+;   installed and if the user doesn't bypass the installation of the
+;   remaining code, execution winds-up here.
+; ---------------------------------------------------------------------------
+loadcode:
+
+; put the other code here.
+
+loadok:
+		mov	si,option_loaded	; let everyone know we're here
+		jmp	optexit
+
+loaderr:
+		mov	si,option_exit	
+; falls through to the exit routine
+optexit:
 		call	bios_puts
-;
-; this is where the option code lies. 
-;
+		int	int19_save		; chain to old INT19h
 		iret
 
-	TIMES EPROM-$+$ db 0
+
+; ---------------------------------------------------------------------------
+; Static Data:	Messages and static data.
+; ---------------------------------------------------------------------------
+option_prompt: 	db 13, 10, "BIOS Extension Test",13, 10
+		db "Press S to skip...",0
+option_skip: 	db "(skipping)", 13, 10, 0
+option_loaded:	db "Extension loaded", 13, 10, 0
+option_exit:	db "Aborting loading", 13, 10, 0
+
+; ---------------------------------------------------------------------------
+; End-of-ROM:	Pad the binary image to the size of the EPROM so that
+;   MkExpRom can place the checksum byte at the end.
+; ---------------------------------------------------------------------------
+;	TIMES EPROM-$+$ db 0
+	TIMES EPROM-($-$$)-11 DB 0
+dt:	db __DATE__		; Assembled date (YYYY-MM-DD)
+;	TIMES EPROM-($-$$) DB 0
+	db  0
+	
 
